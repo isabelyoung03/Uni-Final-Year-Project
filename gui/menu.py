b06fd33a88b777a3f6a49f3eab3e4809279ef11a
isabelyoung03 <isabel.young.2021@live.rhul.ac.uk>
2023-10-04 20:22:45 +0100

    Refactor menu.py, adding method to print text

--- gui/menu.py
+++ gui/menu.py
@@ -11,19 +11,10 @@ def maze_selection_menu():
     selected_maze = None
     menu_screen.fill(config.BLACK)
 
-    title_font = pygame.font.Font('freesansbold.ttf', 20)
-    title = title_font.render('Single-Agent and Multi-Agent Search in Maze Games', True, config.WHITE)
-    titleRect = title.get_rect()
-    titleRect.center = (config.MENU_SCREEN_WIDTH // 2, 50)
-    menu_screen.blit(title, titleRect)
-
-    subtitle_font = pygame.font.Font('freesansbold.ttf', 14)
-    subtitle = subtitle_font.render('By Isabel Young', True, config.WHITE)
-    subtitleRect = subtitle.get_rect()
-    subtitleRect.center = (config.MENU_SCREEN_WIDTH // 2, 125)
-    menu_screen.blit(subtitle, subtitleRect)
-
+    display_text('Single-Agent and Multi-Agent Search in Maze Games', 20, config.WHITE, config.MENU_SCREEN_WIDTH // 2, 50, menu_screen)
     
+    display_text('By Isabel Young', 14, config.WHITE, config.MENU_SCREEN_WIDTH // 2, 125, menu_screen)
+
     transformation = (config.SPRITE_WIDTH*config.PIXEL_SCALE, config.SPRITE_HEIGHT*config.PIXEL_SCALE)
     character = pygame.transform.scale(pygame.image.load("gui/resources/Down.png"), transformation)
     menu_screen.blit(character, dest = (config.MENU_SCREEN_WIDTH // 2 - config.SPRITE_WIDTH, 70))
@@ -37,3 +28,10 @@ def maze_selection_menu():
                     selected_maze = config.SMALL_MAZE_MAP
         pygame.display.flip()
     return selected_maze
+
+def display_text(string, font_size, colour, x, y, screen):
+    font = pygame.font.Font('freesansbold.ttf', font_size)
+    text = font.render(string, True, colour)
+    textRect = text.get_rect()
+    textRect.center = (x, y)
+    screen.blit(text, textRect)
