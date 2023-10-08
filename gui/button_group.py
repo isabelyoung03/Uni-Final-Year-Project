a2e905b38d4d5ea1b25019e075d25070a279ba20
isabelyoung03 <isabel.young.2021@live.rhul.ac.uk>
2023-10-08 17:50:39 +0100

    Add ButtonGroup class and add all buttons on the menu page into groups

--- gui/button_group.py
+++ gui/button_group.py
@@ -3,9 +3,19 @@ from gui.button import Button
 A group of buttons where only one can be selected at a time
 """
 class ButtonGroup():
+    
     def __init__(self, buttons: list = []): 
         self.buttons = buttons
 
+    """
+    Draw each button in the group on the screen
+    """
     def draw(self, screen):
         for button in self.buttons:
             button.draw(screen)
+    """
+    Handle events for each button - usually if they are being pressed
+    """
+    def handle_event(self, event):
+        for button in self.buttons:
+            button.handle_event(event)
