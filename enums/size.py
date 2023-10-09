911c43a9f0913c0b9c2a820389f7592b87a6033d
isabelyoung03 <isabel.young.2021@live.rhul.ac.uk>
2023-10-09 11:21:25 +0100

    Create enums for maze size and search algorithm type

---
+++ enums/size.py
@@ -0,0 +1,8 @@
+from enum import Enum
+"""
+Enum used to define the size of a maze, used as the result from buttons on the menu screen
+"""
+class MazeSize(Enum):
+    SMALL = "Small"
+    MEDIUM = "Medium"
+    LARGE = "Large"
--- gui/button_group.py
+++ gui/button_group.py
@@ -1,4 +1,3 @@
-from gui.button import Button
 """
 A group of buttons where only one can be selected at a time
 """
@@ -6,7 +5,8 @@ class ButtonGroup():
 
     def __init__(self, buttons: list = []): 
         self.buttons = buttons
-        buttons[0].set_selected(True)
+        self.selected_button = buttons[0]
+        self.selected_button.set_selected(True)
 
     """
     Draw each button in the group on the screen
@@ -18,5 +18,35 @@ class ButtonGroup():
     Handle events for each button - usually if they are being pressed
     """
     def handle_event(self, event):
+        # for button in self.buttons:
+        #     button.handle_event(event)
+
+
+        # i = 0
+        # for button in self.buttons:
+        #     if button.handle_event(event):
+        #         button.set_selected(True)
+        #         break
+        #     i += 1
+        # for j in range(len(self.buttons)):
+        #     if i != j:
+        #         self.buttons[j].set_selected(False)
+
         for button in self.buttons:
-            button.handle_event(event)
+                if button.handle_event(event):
+                    self.set_selected(button)
+
+        for button in self.buttons:
+            if button != self.selected_button:
+                button.set_selected(False)
+    
+
+
+    """
+    Get the selected button from this group
+    """
+    # def get_result(self):
+    #     for button in self.buttons:
+    #         if button.get_selected:
+    #             return button. NEED TO MAKE AN ENUM!!!
+
