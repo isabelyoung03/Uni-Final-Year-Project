81fc8dfe407b596192bd0ccd9d944a275dcbb69e
isabelyoung03 <isabel.young.2021@live.rhul.ac.uk>
2024-01-17 16:32:15 +0000

    Make 4 more small mazes and another medium maze

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ MENU_SCREEN_WIDTH, MENU_SCREEN_HEIGHT = 600, 640
 
 # Maze maps
 SMALL_MAZE_SCREEN_WIDTH, SMALL_MAZE_SCREEN_HEIGHT = 1000, 400
-SMALL_MAZE_MAP =    ["+--------------+",
+SMALL_MAZE_MAP_1 =  ["+--------------+",
                     "|        XX    |",
                     "|XXX XXX X  XX |",
                     "|    X X   XXX |",
@@ -12,8 +12,44 @@ SMALL_MAZE_MAP =    ["+--------------+",
                     "|       XX     |",
                     "+--------------+"]
 
+SMALL_MAZE_MAP_2 = ["+--------------+",
+                    "|      XXXXXX  |",
+                    "|X X XX    X  X|",
+                    "|X X    XX X XX|",
+                    "|X XXX         |",
+                    "|      XX XX X |",
+                    "| XXXXX        |",
+                    "+--------------+"]
+
+SMALL_MAZE_MAP_3 = ["+--------------+",
+                    "|  XXX   XXX   |",
+                    "|X     X      X|",
+                    "|X XXX X XXX XX|",
+                    "|X X X X       |",
+                    "|X X     XXXX X|",
+                    "|    XXX       |",
+                    "+--------------+"]
+
+SMALL_MAZE_MAP_4 = ["+--------------+",
+                    "|              |",
+                    "| X X X X X XXX|",
+                    "|         XXX  |",
+                    "| XX X X       |",
+                    "| XX     XXXX X|",
+                    "|    XXX       |",
+                    "+--------------+"]
+
+SMALL_MAZE_MAP_5 = ["+--------------+",
+                    "|              |",
+                    "| XXXXXXXXXXXX |",
+                    "| X   X   X    |",
+                    "|   X   X   X  |",
+                    "| XXXXXXXXXXX X|",
+                    "|              |",
+                    "+--------------+"]
+
 MEDIUM_MAZE_SCREEN_WIDTH, MEDIUM_MAZE_SCREEN_HEIGHT = 1000, 800
-MEDIUM_MAZE_MAP =   ["+--------------+",
+MEDIUM_MAZE_MAP_1 = ["+--------------+",
                     "|        XX    |",
                     "|X XXXXX X  XX |",
                     "|      X   XXX |",
@@ -30,8 +66,25 @@ MEDIUM_MAZE_MAP =   ["+--------------+",
                     "|              |",
                     "+--------------+"]
 
+MEDIUM_MAZE_MAP_2 = ["+--------------+",
+                    "|              |",
+                    "| XX XXXXXXXXXX|",
+                    "| X            |",
+                    "| X XXXXXXXXXX |",
+                    "| X X        X |",
+                    "| X X XXXXXX X |",
+                    "| X X X    X X |",
+                    "| X X X  X X   |",
+                    "| X X XXXX X X |",
+                    "| X X      X X |",
+                    "| X XXXX XXX X |",
+                    "| X          X |",
+                    "| XX XXXXXXXXX |",
+                    "|              |",
+                    "+--------------+"]
+
 LARGE_MAZE_SCREEN_WIDTH, LARGE_MAZE_SCREEN_HEIGHT = 1500, 800
-LARGE_MAZE_MAP =    ["+------------------------+",
+LARGE_MAZE_MAP_1 =  ["+------------------------+",
                     "|             X        X |",
                     "| XXX XXX XXXXXX XXXXX X |",
                     "| X X X             XX X |",
--- src/enums/size.py
+++ src/enums/size.py
@@ -4,9 +4,9 @@ import config
 Enum used to define the size of a maze, used as the result from buttons on the menu screen
 """
 class MazeSize(Enum):
-    SMALL = config.SMALL_MAZE_MAP
-    MEDIUM = config.MEDIUM_MAZE_MAP
-    LARGE = config.LARGE_MAZE_MAP
+    SMALL = config.SMALL_MAZE_MAP_2
+    MEDIUM = config.MEDIUM_MAZE_MAP_2
+    LARGE = config.LARGE_MAZE_MAP_1
 
     """
     Return the width of the screen for this size maze
