2ffee1d7f42e54741905717ac0f2865c02910cf1
isabelyoung03 <isabel.young.2021@live.rhul.ac.uk>
2024-01-26 11:46:52 +0000

    Add button for selecting number of opponents for minimax and expectimax

--- src/gui/menu.py
+++ src/gui/menu.py
@@ -14,7 +14,6 @@ class Menu:
         small = OptionButton('Small', 20, config.GREEN, config.BLACK, 140, 240, MazeSize.SMALL)
         medium = OptionButton('Medium', 20, config.GREEN, config.BLACK, config.MENU_SCREEN_WIDTH // 2 - 40, 240, MazeSize.MEDIUM)
         large = OptionButton('Large', 20, config.GREEN, config.BLACK, 400, 240, MazeSize.LARGE)
-
         self.size_button_group = ButtonGroup([small, medium, large])
 
         breadth = OptionButton('Breadth-first', 20, config.GREEN, config.BLACK, 85, 355, SearchAlgoType.BREADTH)
@@ -26,11 +25,14 @@ class Menu:
         minimax = OptionButton('Minimax', 20, config.GREEN, config.BLACK, 90, 475, SearchAlgoType.MINIMAX)
         expectimax = OptionButton('Expectimax', 20, config.GREEN, config.BLACK, config.MENU_SCREEN_WIDTH // 2 - 60, 475, SearchAlgoType.EXPECTIMAX)
         reflex = OptionButton('Reflex', 20, config.GREEN, config.BLACK, 435, 475, SearchAlgoType.REFLEX)
-
         self.algo_button_group = ButtonGroup([breadth, depth, uniform, a_star, a_star_all_cells, greedy, minimax, expectimax, reflex])
 
-        self.start_button = Button('Start', 20, config.BLACK, config.WHITE, 250, 540, 100, 50)
+        one = OptionButton('1', 20, config.GREEN, config.BLACK, 95, 570, 1)
+        two = OptionButton('2', 20, config.GREEN, config.BLACK, 125, 570, 2)
+        three = OptionButton('3', 20, config.GREEN, config.BLACK, 155, 570, 3)
+        self.opponent_button_group = ButtonGroup([one, two, three])
 
+        self.start_button = Button('Start', 20, config.BLACK, config.WHITE, 250, 540, 100, 50)
         self.menu_screen = pygame.display.set_mode((config.MENU_SCREEN_WIDTH, config.MENU_SCREEN_HEIGHT))
 
     """
@@ -48,12 +50,14 @@ class Menu:
                     if self.start_button.rectangle.collidepoint(event.pos): #if start button is pressed
                         selected_maze_size = self.size_button_group.get_result()
                         selected_search_algo = self.algo_button_group.get_result()
+                        no_of_opponents = self.opponent_button_group.get_result()
                         maze = Maze(selected_maze_size)
                 self.size_button_group.handle_event(event)
                 self.algo_button_group.handle_event(event)
+                self.opponent_button_group.handle_event(event)
             self.draw()
             pygame.display.flip()
-        return (maze, selected_search_algo)
+        return (maze, selected_search_algo, no_of_opponents)
 
     """
     Draw the menu screen
@@ -69,11 +73,16 @@ class Menu:
         display_text('By Isabel Young', 16, config.WHITE, config.MENU_SCREEN_WIDTH // 2, 125, self.menu_screen)
         display_text('Select maze size', 16, config.WHITE, config.MENU_SCREEN_WIDTH // 2, 200, self.menu_screen)
         display_text('Select agent type', 16, config.WHITE, config.MENU_SCREEN_WIDTH // 2, 315, self.menu_screen)
-        
+
         self.size_button_group.draw(self.menu_screen)
         self.algo_button_group.draw(self.menu_screen)
 
         self.start_button.draw(self.menu_screen)
+
+        selected_algo = self.algo_button_group.get_result()
+        if selected_algo in [SearchAlgoType.MINIMAX, SearchAlgoType.EXPECTIMAX]:
+            display_text('Opponents', 16, config.WHITE, 132, 550, self.menu_screen)
+            self.opponent_button_group.draw(self.menu_screen)
 
 """
 Display text string on the given screen, of a particular size and colour at location x,y
